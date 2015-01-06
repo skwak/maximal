7c60b522749a7400dd644a919217c871b1898f4c
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-01-06 15:10:57 -0800

    tumblr login works mostly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,16 @@
 class SessionsController < ApplicationController
-  def create_oauth
-  end
-end
 
-# auth_hash = request.env['omniauth.auth']
-# if Oauth.find_by(uid: auth_hash["uid"])
-#   @oauth = Oauth.find_by(uid: auth_hash["uid"])
-#   session[:user_id] = @oauth.user_id
-#   @user = User.find(@oauth.user_id)
-#   redirect_to my_account_path, notice: "You're signed in, #{@user.username}. Yay!"
-# else
-#   @user = User.create(username: auth_hash["info"]["nickname"], password_digest: "nil", email: "nil")
-#   @oauth = Oauth.create(user_id: @user.id, uid: auth_hash["uid"], provider: auth_hash["provider"], token: auth_hash["credentials"]["token"])
-#   session[:user_id] = @oauth.user_id
-#   redirect_to my_account_path, notice: "You're signed in, #{@user.username}. Yay!"
-# end
+  def create_oauth
+    auth_hash = request.env["omniauth.auth"]
+    if User.find_by(uid: auth_hash["uid"]).nil?
+      @user = User.create(uid: auth_hash["uid"], name: auth_hash["info"]["name"], provider: params[:provider], avatar: auth_hash["info"]["avatar"])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "You're signed in, #{@user.name}"
+    else
+      @user = User.find_by(uid: auth_hash["uid"])
+      session[:user_id] = @user.id
+      redirect_to root_path, flash: "Something went wrong :-("
+    end
+  end
+  
+end
