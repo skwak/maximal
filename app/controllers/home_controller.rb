9b23360142ec4c380826ebc11c9e93018f7e9c1a
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-01-07 12:34:58 -0800

    parsing through tumblr api chaos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+    @tumblr_user = current_user.name
+    @tumblrs = HTTParty.get("http://api.tumblr.com/v2/blog/#{@tumblr_user}.tumblr.com/posts?api_key=#{ENV["TUMBLR_KEY"]}&limit=10").parsed_response
+    @tumblr_posts = @tumblrs["response"]["posts"]
+    @tumblrs = @tumblr_posts.collect { |post| post["date"] + ", " + post["post_url"] + ", " + post["photos"].to_json }
   end
 
 end
