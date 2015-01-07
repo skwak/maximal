0794732a5b80092fc7ef6a62d1b0130fc01ebd66
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-01-07 14:04:55 -0800

    can plop a background image into canvas

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -1,7 +1,17 @@
 $(function() {
   sketch();
+  drag();
 });
 
 function sketch() {
   $("#collage-spot").sketch();
-}
+};
+
+function drag() {
+  $(".image-bucket").draggable({
+    containment: $("#collage-spot"),
+    tolerance: 'fit',
+    cursor: 'pointer',
+    position: 'relative' 
+  });
+};
