11d15cca0887ed463fc535baf5c7c9bc274c6c08
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-01-06 16:02:12 -0800

    tiny styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,12 @@ class SessionsController < ApplicationController
     if User.find_by(uid: auth_hash["uid"]).nil?
       @user = User.create(uid: auth_hash["uid"], name: auth_hash["info"]["name"], provider: params[:provider], avatar: auth_hash["info"]["avatar"])
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "You're signed in, #{@user.name}"
+      redirect_to root_path
     else
       @user = User.find_by(uid: auth_hash["uid"])
       session[:user_id] = @user.id
-      redirect_to root_path, flash: "Something went wrong :-("
+      redirect_to root_path
     end
   end
-  
+
 end
