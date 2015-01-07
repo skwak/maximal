a10001271a528dd638be83d5386fd458698f160c
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-01-06 16:46:27 -0800

    finish signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,4 +18,9 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Signed out."
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get "auth/:provider/callback", to: "sessions#create_oauth"
 
+  delete "/signout", to: "sessions#destroy", as: :signout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
