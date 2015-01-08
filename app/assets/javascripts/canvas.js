9a75e268078f4cf17d95ff404698b773f8cd4629
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-01-08 15:30:59 -0800

    comment out js for now

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -1,40 +1,39 @@
 $(function() {
-  sketch();
+  // sketch();
   // imageResize();
-  drag();
-  dropImage();
+  // drag();
+  // dropImage();
 });
 
-function sketch() {
-  $("#collage-spot").sketch();
-}
+// function sketch() {
+//   $("#collage-spot").sketch();
+// }
 
-function drag() {
-  $(".selected-image").draggable({
-    // containment: $("#collage-spot"),
-    // tolerance: 'fit',
-    // cursor: 'pointer',
-    // position: 'relative',
-    rever: "valid",
-    helper: "clone"
-    // stop: handleDragStop
-  });
-}
+// function drag() {
+//   $(".selected-image").draggable({
+    // containment: $(".container"),
+//     revert: "invalid",
+//     helper: "clone",
+//   });
+// }
+
+// function dropImage() {
+//   $("#collage-spot").droppable({
+//     accept: $("#selections .selected-image"),
+//     drop: function (event, ui) {  
+//       $(this).append($(ui.helper).clone().draggable({
+//         containment: "parent"
+//       }));
+//     }
+//   });
+// }
 
 // function handleDragStop( event, ui ) {
 //   var offsetXPos = parseInt( ui.offset.left );
 //   var offsetYPos = parseInt( ui.offset.top );
-//   alert( "Drag stopped!\n\nOffset: (" + offsetXPos + ", " + offsetYPos + ")\n");
+  // alert( "Drag stopped!\n\nOffset: (" + offsetXPos + ", " + offsetYPos + ")\n");
 // }
-// 
 
-function dropImage() {
-  $("#collage-spot").droppable({
-    // accept: $(".selected-image"),
-    drop: function (e, ui){  
-      $(ui.draggable).clone().appendTo($(this));
-    } 
-  });
-}
+
   
   
